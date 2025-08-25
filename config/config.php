ad73178a6a3c5ec70487760b55ca8736bde152d5
helloakshay27 <helloakshay27@gmail.com>
2025-08-25 22:34:40 +0530

    new

---
+++ config/config.php
@@ -0,0 +1,23 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ocd4v630jj61',
+  'passwordsalt' => 'ckF0k/ZNdXqA+v16VlMAqVZR4OC5hH',
+  'secret' => 'iUELGLR5EOalb1c6qGJTcwmByGBxsJmpIpy9zXn7s2dDBK7o',
+  'trusted_domains' => 
+  array (
+    0 => 'localhost:8080',
+  ),
+  'datadirectory' => '/Users/haveninfoline/Desktop/php/nextcloud/data',
+  'dbtype' => 'mysql',
+  'version' => '31.0.8.1',
+  'overwrite.cli.url' => 'http://localhost:8080',
+  'dbname' => 'test_db',
+  'dbhost' => '127.0.0.1',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'root',
+  'dbpassword' => 'Shindeak51@',
+  'installed' => true,
+  'maintenance' => false,
+);
